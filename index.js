fc380d06a6f8d446e4da3aa92d9369a438bf4340
Jasmin <ssongnari@gmail.com>
2022-04-13 05:41:08 -0400

    commit tree 3cab2872a66d80249b45a9c9804fd6c5d7f85dc2

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+// Write your algorithm here
+// const arrayInOrder = array.sort();
+// const half = Math.ceil(arrayInOrder.length / 2);
+// const firstHalf = arrayInOrder.splice(0, half)
+// const secondHalf = arrayInOrder.splice(-half)
+// for (let i = 0; i < firstHalf.length; i++) {
+//   for (let j = 0; j < secondHalf.length; j++) {
+//     if (firstHalf[i] + secondHalf[j] === target) {
+//       return true
+//     }
+//     else { return false }
+//   }
+// }
+// if (firstHalf[0] + secondHalf[secondHalf.length] === target){
+//   return true
+// }
+// else (fi)
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,11 +32,23 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+// put the array in numerical order
+cut the array in half
+if the index0 number from arrayA and the index[array.length] number from arrayB === target, return true
+if the next index number and the index[array.length-1] number === target, return true
+so on and so forth
 
 /*
   Add written explanation of your solution here
 */
+
+
+
+
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
